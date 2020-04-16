fc7077a738a179e56537f95285c311accf5bdbf5
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-16 21:51:33 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  array.max { |a, b|  }
+  counter = 0
+  mv = 1
+  while counter < array.length do
+    if mv > array[counter]
+      mv = array[counter]
+      
+    end
+    counter +=  
+  end
 end
